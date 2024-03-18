d3c9fd5f61d0be5f4dce877d4a996d2d8785402b
asbel-software-engineer <asbelkipkosgei0@gmail.com>
2024-03-17 23:30:48 -0700

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if 'a' <= char <= 'z':
+            upper = chr(ord(char) - 32)
+        else:
+            upper = char
+        print("{}" .format(upper), end="")

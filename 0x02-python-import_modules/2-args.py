61adee9d9ea9ab76ea260f3cdac9170cd788e464
asbel-software-engineer <asbelkipkosgei0@gmail.com>
2024-03-19 16:33:49 -0700

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+def print_arguments():
+    args = sys.argv[1:]
+    num_args = len(args)
+    
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:" .format(num_args))
+        for i, arg in enumerate(args, 1):
+            print(f"{i}: {arg}")
+
+if __name__ == "__main__":
+    print_arguments()
+

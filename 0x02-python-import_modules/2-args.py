5d945e92369f1a59ef672ce79bf5bd83e1433698
asbel-software-engineer <asbelkipkosgei0@gmail.com>
2024-03-19 16:46:03 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,10 @@ def print_arguments():
     num_args = len(args)
     if num_args == 0:
         print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+        for i, arg in enumerate(args, 1):
+            print(f"{i}: {arg}")
     else:
         print("{} arguments:" .format(num_args))
         for i, arg in enumerate(args, 1):

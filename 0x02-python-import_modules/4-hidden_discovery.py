a86a2ba482ba6165f877350b5c6a5c3ec791bdee
asbel-software-engineer <asbelkipkosgei0@gmail.com>
2024-03-19 18:08:14 -0700

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import importlib.util
 
-def name_of_module(module_name):
-    spec = importlib.util.spec_from_file_location(module_name, f"{module_name}.pyc")
+
+def name_of_module(name_m):
+    spec = importlib.util.spec_from_file_location(name_m, f"{name_m}.pyc")
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
-    
     names = [name for name in dir(module) if not name.startswith("__")]
     names.sort()
-    
     for name in names:
         print(name)
 
+
 if __name__ == "__main__":
-    print_names_of_module("hidden_4")
+    name_of_module("hidden_4")

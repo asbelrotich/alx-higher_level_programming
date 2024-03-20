f8fdd94c620b3ec8874bdb7445b5588a1e3dce2b
asbel-software-engineer <asbelkipkosgei0@gmail.com>
2024-03-20 14:41:31 -0700

    git commit -m

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     b = len(my_list)
     if idx < 0:
         return None
-    elif idx > b:
+    elif idx >= b:
         return None
     else:
         return my_list[idx]

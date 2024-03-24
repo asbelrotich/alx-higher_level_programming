fb60dd09633a2cb713b130c8eaa91de68908a4f6
asbel-software-engineer <asbelkipkosgei0@gmail.com>
2024-03-24 07:31:29 -0700

    swuare matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
-        squared_row = []
-        for element in row:
-            squared_row.append(element ** 2)
-        new = matrix.append(squared_row)
+    new_matrix = matrix.copy()
+    for row in range(len(matrix)):
+        new_matrix[row] = list(map(lambda x: x**2, matrix[row]))
     return new_matrix
